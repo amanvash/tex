6c064d844499e61a93b779fbc32a75708ce5356f
censorday <74760933+censorday@users.noreply.github.com>
2020-12-04 15:02:58 +0530

    Create ind.php

---
+++ ind.php
@@ -0,0 +1,29 @@
+<html>
+<title>Canva</title>
+<style type="text/css">
+textarea {
+ background: url(http://i.stack.imgur.com/ynxjD.png) repeat-y;
+ width: 600px;
+ height: 300px;
+ font: normal 14px verdana;
+ line-height: 25px;
+ padding: 2px 10px;
+ border: solid 1px #ddd;
+}
+
+</style>
+
+<body>
+<textarea>
+    Textarea with style example
+    Line 1
+    Line 2
+    Line 3
+    Line 4
+    Line 5
+    Line 6
+    Line 7
+    Line n
+</textarea>
+</body>
+</html>
